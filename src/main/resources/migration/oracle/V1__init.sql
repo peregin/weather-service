0bcf50c853dc6139b7a25104ecf042a6dc3320f8
peregin <velocorner.com@gmail.com>
2025-04-14 17:38:43 +0200

    setup oracle migration [skip ci]

--- src/main/resources/migration/oracle/V1__init.sql
+++ src/main/resources/migration/oracle/V1__init.sql
@@ -1,15 +1,19 @@
 create table forecast(
     location varchar2(100) not null,
-    update_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT SYSTIMESTAMP,
+    update_time TIMESTAMP DEFAULT SYSTIMESTAMP NOT NULL,
     data CLOB NOT NULL CHECK (data IS JSON),
     primary key(location, update_time)
-);
+) segment creation immediate
+    nocompress logging
+    tablespace "WEATHER";
 
 create table weather(
     location varchar2(100) not null,
     data CLOB NOT NULL CHECK (data IS JSON),
     primary key(location)
-);
+) segment creation immediate
+    nocompress logging
+    tablespace "WEATHER";
 
 comment on table weather is 'stores the current weather conditions';
 comment on table forecast is 'stores the future - forecasted - weather conditions';
