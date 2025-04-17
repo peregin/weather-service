086badb829c7fc12b97a5d7943ea2daf2594f9d3
levi <velocorner.com@gmail.com>
2025-04-17 22:02:54 +0200

    test oracle [skip ci]

--- osql23ai.sh
+++ osql23ai.sh
@@ -7,10 +7,10 @@ if [[ $(docker inspect -f '{{.State.Running}}' $CONTAINER_REPO) = "true" ]]; the
   echo "$CONTAINER_REPO is already running ..."
 else
   echo home directory is "$HOME"
-  docker run -d -p 1522:1521 -p 5500:5500 \
+  docker run -d -p 1521:1521 -p 5500:5500 \
       --rm --name $CONTAINER_REPO \
       --security-opt label=disable \
-      --shm-size=1g \
+      --shm-size=2g \
       -e ORACLE_PWD=password \
       -v /opt/oracle/oradata23:/opt/oracle/oradata \
       container-registry.oracle.com/database/free:latest
