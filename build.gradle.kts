c71214f2cf9e6ea62bd081f67f30a89c278cef9b
peregin <velocorner.com@gmail.com>
2025-05-14 18:27:17 +0200

    bump versions [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.OffsetDateTime
 
 val ktor_version: String by project
 val kotlin_version: String by project
-val plugin_version = "2.1.20"
+val plugin_version = "2.1.21"
 val openapi_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
@@ -17,8 +17,8 @@ version = "1.0.1-SNAPSHOT"
 
 plugins {
     application
-    kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
     id("name.remal.check-updates") version "1.5.0"
     id("io.ktor.plugin") version "3.1.3"
     id("org.graalvm.buildtools.native") version "0.10.6"
